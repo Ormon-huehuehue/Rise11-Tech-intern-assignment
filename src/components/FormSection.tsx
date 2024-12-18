2add9f2a9989d97dbe0bd496351d31c77ecbdc0c
Ormon-huehuehue <armnvrma10@gmail.com>
2024-12-19 00:10:09 +0530

    final touches

--- src/components/FormSection.tsx
+++ src/components/FormSection.tsx
@@ -4,7 +4,7 @@ import ClaimForm from './ClaimForm'
 
 const FormSection = () => {
   return (
-    <div className=" w-full xl:h-[calc(100vh-105px)] h-auto bg-background p-5 rounded-3xl flex flex-col justify-between">
+    <div className=" w-full h-auto bg-background p-5 rounded-3xl flex flex-col justify-between mb-3 mx-4">
         <ProgressBar/>
         <ClaimForm/>
     </div>
