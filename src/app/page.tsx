9ff10393117f953e119dd71a167f1ad8fba8668c
Ormon-huehuehue <armnvrma10@gmail.com>
2024-12-18 22:34:29 +0530

    fixing build errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import FormSection from "@/components/FormSection";
 import SideBar from "@/components/Sidebar";
-import Image from "next/image";
 
 export default function Home() {
   return (
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -7,41 +7,33 @@ import { LuFileSliders } from "react-icons/lu";
 import { IoHandLeftOutline } from "react-icons/io5";
 
 const SideBar= () => {
-  const Items: string[] = [
-    "Dashboard",
-    "My Cases",
-    "Activities",
-    "Calender",
-    "Files",
-    " Open a dispute",
-  ];
 
   const image= "./RandomAhhImage.png"
 
   return (
     <div className=" hidden sm:hidden md:flex lg-flex xl:flex 2xl:flex flex-col w-[300px]  font-montserrat font-semibold justify-between">
-      <ul className=" flex flex-col justify-center items-center mt-[5vh] text-sidebarText">
-        <li className="w-full py-4 pl-8 cursor-pointer text-gray-500 hover:bg-gray-100 text-lg flex gap-4">
+      <ul className=" flex flex-col justify-center items-center mt-[5vh] text-sidebarText text-sm xl:text-lg">
+        <li className="w-full py-4 pl-8 cursor-pointer text-gray-500 hover:bg-gray-100 flex gap-4">
           <TbDashboard size="28px" color="#80aee2"/>
           <h1 className=  'flex items-center'> Dashboard</h1>
         </li>
-        <li className="w-full py-4 pl-8 cursor-pointer text-gray-500 hover:bg-gray-100 text-lg flex gap-4">
+        <li className="w-full py-4 pl-8 cursor-pointer text-gray-500 hover:bg-gray-100 flex gap-4">
           <PiSuitcaseSimpleBold size="28px" color="#80aee2"/>
           <h1 className=  'flex items-center'> My Cases</h1>
         </li>
-        <li className="w-full py-4 pl-8 cursor-pointer text-gray-500 hover:bg-gray-100 text-lg flex gap-4">
+        <li className="w-full py-4 pl-8 cursor-pointer text-gray-500 hover:bg-gray-100 flex gap-4">
           <TbWaveSawTool size="28px" color="#80aee2"/>
           <h1 className=  'flex items-center'> Activity</h1>
         </li>
-        <li className="w-full py-4 pl-8 cursor-pointer text-gray-500 hover:bg-gray-100 text-lg flex gap-4">
+        <li className="w-full py-4 pl-8 cursor-pointer text-gray-500 hover:bg-gray-100 flex gap-4">
           <BsCalendar3 size="28px" color="#80aee2"/>
           <h1 className=  'flex items-center'> Calendar</h1>
         </li>
-        <li className="w-full py-4 pl-8 cursor-pointer text-gray-500 hover:bg-gray-100 text-lg flex gap-4">
+        <li className="w-full py-4 pl-8 cursor-pointer text-gray-500 hover:bg-gray-100 flex gap-4">
           <LuFileSliders size="28px" color="#80aee2"/>
           <h1 className=  'flex items-center'> Files</h1>
         </li>
-        <li className="w-full py-4 pl-8 cursor-pointer text-gray-500 hover:bg-gray-100 text-lg flex gap-4">
+        <li className="w-full py-4 pl-8 cursor-pointer text-gray-500 hover:bg-gray-100 flex gap-4">
           <IoHandLeftOutline size="28px" color="#80aee2"/>
           <h1 className=  'flex items-center'> Dispute</h1>
         </li>
